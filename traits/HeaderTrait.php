20bd260e68a2eadd8fb954c907c8307157ec76da
SID <kormendy.csaba@gmail.com>
2026-05-02 09:08:37 +0200

    # modify Header Trait global language

--- traits/HeaderTrait.php
+++ traits/HeaderTrait.php
@@ -12,7 +12,7 @@ trait HeaderTrait
     {
         $htmlHeader = '
             <!doctype html>
-            <html lang="en">
+            <html lang="hu">
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
